2a2d2d50000e4cb0f7574feb827400bb5e656243
Jocelyn Ashwell <jocelynashwell@gmail.com>
2014-12-29 13:24:03 -0500

    User sign-in & out

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blocitoff::Application.config.secret_key_base = '9e11b83f6be3f5a46c2fc549301bf8e5780151bdb18d452a595d0c684b8ef1268084019676b6d3d4426c1ad4a17fa9cf7821653308108de61daffa406b450aa1'
+Blocitoff::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
