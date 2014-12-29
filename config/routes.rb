03daa4d6e0271227ff7dfaf18e4736f5d5389bd8
Jocelyn Ashwell <jocelynashwell@gmail.com>
2014-12-29 09:52:13 -0500

    Created welcome controller w/ index & views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blocitoff::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/about"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
